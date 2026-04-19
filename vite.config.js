93c1a74bd09f2bbc5e5126083df08587c451427c
Sourav Jha <souravkjha2007@gmail.com>
2026-04-20 02:31:23 +0530

    chore: add setupTests.js to Vitest configuration

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
   build: {
